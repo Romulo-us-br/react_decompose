21f2618cdbab9cb1dd8f1b57dae5caf4cd8e91cd
Romulo Brasil <romulop.brasil25@gmail.com>
2026-05-20 20:45:51 -0300

    t1

--- src/components/Article/Article.spec.jsx
+++ src/components/Article/Article.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react18';
-import { Article } from './Article';
+import  Article  from './Article';
 
 describe('Article component', () => {
   beforeEach(() => {
--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react18';
-import { Welcome } from './Welcome';
+import  Welcome  from './Welcome';
 
 describe('Welcome component', () => {
   beforeEach(() => {
